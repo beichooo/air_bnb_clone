df9b0efbc3233bca153cf4a8f3c9bbfc54b61797
beichooo <be.imagine.redo@gmail.com>
2023-12-26 00:22:00 -0400

    navbar-ui-complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar/navbar";
+import Navbar from "@/app/components/navbar/Navbar";
 
 const nunitoFont = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AirBNB Clone",
+  title: "AirBNB Clone for your MVP",
   description: "A project to learn how create my startup",
 };
 
--- components/navbar/logo.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-const Logo = () => {
-  return <div>hello logo</div>;
-};
-
-export default Logo;
